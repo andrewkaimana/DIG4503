d2db6e82553d7920f55aa2a26eed7c11b4832639
Andrew Kaimana <andrew.kaimana@knights.ucf.edu>
2021-02-05 15:52:13 -0500

    Commit Activity4

--- activity4/src/index.js
+++ activity4/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const customer = {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  first_name: "Alex",
+
+  last_name: "DeVille"
+
+};
+const output = <div>
+  
+<h1>My name is {customer.first_name}</h1>
+<h2>My last name is {customer.last_name}</h2>
+
+</div>;
+
+ReactDOM.render(output, document.querySelector("#root"));
